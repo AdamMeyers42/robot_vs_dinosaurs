47b4c90643d081c447e44d2a936cab4182fc06dc
AdamMeyers42 <adammeyers3551@gmail.com>
2021-10-06 11:05:58 -0700

    Created all py files for classes

---
+++ robots.py
@@ -0,0 +1,10 @@
+from weapons import Weapon
+
+class Robot:
+    def __init__(self, name):
+        self.name = name
+        self.health = 0
+        self.weapon = '' 
+
+    def robot_attack(self):
+        pass
